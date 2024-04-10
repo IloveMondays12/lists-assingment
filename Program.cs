dc172f17858d57558cd2af4120f9e425923d3f60
Ethan Jones <339467243@GoTVDSB.ca>
2024-04-10 13:17:50 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists__lesson_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random generator = new Random();
+           List <int> numRange = new List<int>();
+            for (int i = 0; i < 25;)
+            {
+             numRange.Add[generator.Next(10,20)];
+            }
+
+        }
+    }
+}
